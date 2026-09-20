6480b1d10d89228c570c1f9ff3292cfb9ff4e54c
guilhermebaez <guilhermedevgml@gmail.com>
2026-09-20 14:07:23 -0300

    feat: melhora visual e interatividade do dashboard

---
+++ Dashboard/scriptDashboard.js
@@ -0,0 +1,9 @@
+console.log("JavaScript do Dashboard conectado!");
+
+// Pega os números dos cards pelo ID
+const total = document.getElementById("total");
+const abertas = document.getElementById("abertas");
+const andamento = document.getElementById("andamento");
+const revisao = document.getElementById("revisao");
+const concluidas = document.getElementById("concluidas");
+const canceladas = document.getElementById("canceladas");
